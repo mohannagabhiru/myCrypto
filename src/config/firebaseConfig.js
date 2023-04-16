bcb77d778c837f704feed56ff2e2ae6dd6216dae
mohan nagabhiru <mohannagabhiru@gmail.com>
2023-04-17 01:35:20 +0530

    Final project

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz0yqlFeP-9PwgK0tNJvQRLlkQpFBwcYg",
+  authDomain: "allcrypto-b7b0e.firebaseapp.com",
+  projectId: "allcrypto-b7b0e",
+  storageBucket: "allcrypto-b7b0e.appspot.com",
+  messagingSenderId: "1041810137812",
+  appId: "1:1041810137812:web:4760ffe0ef1f3a05925141"
+};
+
+
+export default firebaseConfig;
